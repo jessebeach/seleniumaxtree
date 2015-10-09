9706d7ce22c51736b8e31ce609a4c98b0544343c
Jesse Renee Beach <splendidnoise@gmail.com>
2015-10-09 10:22:35 -0700

    Updated Chrome extension

--- tests/ax.js
+++ tests/ax.js
@@ -8,7 +8,28 @@ module.exports = {
     client
       .page.ax
       .load()
-      .saveScreenshot('./results/screenshots/screen2.png')
-      .end();
+      .saveScreenshot('./results/screenshots/screen2.png');
+
+    client
+      .timeoutsAsyncScript(50)
+      .executeAsync(function (done, data) {
+        function onMessage(event) {
+          var data = event.data;
+          if (
+            data.source === 'extension' &&
+            data.extension === 'arborea11y'
+          ) {
+            done(data.axtree);
+          }
+        }
+        window.addEventListener('message', onMessage, false);
+        window.postMessage({
+          extension: "arborea11y",
+          source: 'page',
+          action: 'message'
+        }, "*");
+      }, [], function (response) {
+        console.log(response.value);
+      });
   }
 };
